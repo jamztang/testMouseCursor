3a9a57b4e8f2e9a88d7e63edd08246a6698d801b
James Tang <james@goodnotesapp.com>
2020-06-12 13:10:40 +0800

    Add crosshair auto showing within 7 pt

--- testMouseCursor/ViewController.swift
+++ testMouseCursor/ViewController.swift
@@ -73,46 +73,26 @@ class ViewController: UIViewController {
         let accessibilityMouseTransform = accessibility?["mouseDriverCursorSize"] as? CGFloat ?? 1
         let mouseTransform = catalystTransform / accessibilityMouseTransform
 
-        let crosshairStrokeLength: CGFloat = 9
+        let circlePadding: CGFloat = 9 / catalystTransform
         var size = CGSize(width: length, height: length).applying(.init(scaleX: mouseTransform, y: mouseTransform))
 
         // Adding spaces for crosshair
-        size.width += crosshairStrokeLength * 2
-        size.height += crosshairStrokeLength * 2
+        size.width += circlePadding * 2
+        size.height += circlePadding * 2
+
+        let crosshairStrokeLength: CGFloat = 4 / catalystTransform
+        let crosshairStrokeWidth: CGFloat = 2 / catalystTransform
+        let crosshairStrokePadding: CGFloat = 3 / catalystTransform
+
+        let showCrosshair = length * catalystTransform < 7
 
         let image = try? UIImage.draw(size: size, scale: accessibilityMouseTransform) { (context, size, scale) in
-
-////            context.setFillColor(UIColor.yellow.cgColor)
-////            context.addRect(CGRect(x: 0, y: 0, width: size.width, height: size.height))
-//            context.drawPath(using: CGPathDrawingMode.fill)
-////            context.stroke(CGRect(x: 0, y: 0, width: size.width, height: size.height), width: 1)
-//
-//            context.setFillColor(UIColor.black.cgColor)
-//            context.addRect(CGRect(x: size.width / 2 - 1, y: 2, width: 1, height: 3))
-//            context.setStrokeColor(UIColor.white.cgColor)
-//            context.addPath(CGPath(roundedRect: CGRect(x: size.width / 2 - 1.5, y: 1, width: 3, height: 5), cornerWidth: 1, cornerHeight: 1, transform: nil))
-//            context.strokePath()
-//
-//            let strokeWidth: CGFloat = size.width - (2 * crosshairStrokeLength)
-//            let color: UIColor = .green
-//            let rectangle = CGRect(x: crosshairStrokeLength,
-//                                   y: crosshairStrokeLength,
-//                                   width: strokeWidth,
-//                                   height: strokeWidth)
-//
-//            Swift.print("XXX mouse.changeCursor(.image) size \(size) \(scale) \(rectangle)")
-//            context.setFillColor(color.cgColor)
-//            context.setShouldAntialias(true)
-//            context.setAllowsAntialiasing(true)
-//            context.addEllipse(in: rectangle)
-//            context.drawPath(using: CGPathDrawingMode.fill)
 
             let frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
 
             func fastFloor(_ x: CGFloat) -> CGFloat { return floor(x) }
 
             //// Color Declarations
-            let color = UIColor(red: 0.863, green: 0.863, blue: 0.863, alpha: 1.000)
             let color2 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1.000)
 
             //// Shadow Declarations
@@ -121,23 +101,19 @@ class ViewController: UIViewController {
             shadow.shadowOffset = CGSize(width: 0, height: 1)
             shadow.shadowBlurRadius = 2
 
-//            //// Rectangle 3 Drawing
-//            let rectangle3Path = UIBezierPath(rect: CGRect(x: frame.minX, y: frame.minY, width: frame.width, height: frame.height))
-//            color.setFill()
-//            rectangle3Path.fill()
-//
-
             //// Circle Drawing
-            let circlePath = UIBezierPath(ovalIn: CGRect(x: frame.minX + 9, y: frame.minY + 9, width: frame.width - 18, height: frame.height - 18))
+            let circlePath = UIBezierPath(ovalIn: CGRect(x: frame.minX + circlePadding, y: frame.minY + circlePadding, width: frame.width - circlePadding * 2, height: frame.height - circlePadding * 2))
             UIColor.gray.setFill()
             circlePath.fill()
             UIColor.white.setStroke()
-            circlePath.lineWidth = 1
+            circlePath.lineWidth = 1 / scale
             circlePath.stroke()
 
 
+            guard showCrosshair else { return }
+
             //// T Drawing
-            let tPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + fastFloor((frame.width - 2) * 0.50000 + 0.5), y: frame.minY + 2, width: 2, height: 5), cornerRadius: 0.5)
+            let tPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + fastFloor((frame.width - crosshairStrokeWidth) * 0.50000 + 0.5), y: frame.minY + crosshairStrokePadding, width: crosshairStrokeWidth, height: crosshairStrokeLength), cornerRadius: 0.5)
             context.saveGState()
             context.setShadow(offset: shadow.shadowOffset, blur: shadow.shadowBlurRadius, color: (shadow.shadowColor as! UIColor).cgColor)
             color2.setFill()
@@ -145,12 +121,12 @@ class ViewController: UIViewController {
             context.restoreGState()
 
             UIColor.white.setStroke()
-            tPath.lineWidth = 1
+            tPath.lineWidth = 1 / catalystTransform
             tPath.stroke()
 
 
             //// B Drawing
-            let bPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + fastFloor((frame.width - 2) * 0.50000 + 0.5), y: frame.minY + frame.height - 7, width: 2, height: 5), cornerRadius: 0.5)
+            let bPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + fastFloor((frame.width - crosshairStrokeWidth) * 0.50000 + 0.5), y: frame.minY + frame.height - (crosshairStrokePadding + crosshairStrokeLength), width: crosshairStrokeWidth, height: crosshairStrokeLength), cornerRadius: 0.5)
             context.saveGState()
             context.setShadow(offset: shadow.shadowOffset, blur: shadow.shadowBlurRadius, color: (shadow.shadowColor as! UIColor).cgColor)
             color2.setFill()
@@ -158,12 +134,12 @@ class ViewController: UIViewController {
             context.restoreGState()
 
             UIColor.white.setStroke()
-            bPath.lineWidth = 1
+            bPath.lineWidth = 1 / catalystTransform
             bPath.stroke()
 
 
             //// R Drawing
-            let rPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + frame.width - 7, y: frame.minY + fastFloor((frame.height - 2) * 0.50000 + 0.5), width: 5, height: 2), cornerRadius: 0.5)
+            let rPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + frame.width - (crosshairStrokePadding + crosshairStrokeLength), y: frame.minY + fastFloor((frame.height - crosshairStrokeWidth) * 0.50000 + 0.5), width: crosshairStrokeLength, height: crosshairStrokeWidth), cornerRadius: 0.5)
             context.saveGState()
             context.setShadow(offset: shadow.shadowOffset, blur: shadow.shadowBlurRadius, color: (shadow.shadowColor as! UIColor).cgColor)
             color2.setFill()
@@ -171,12 +147,12 @@ class ViewController: UIViewController {
             context.restoreGState()
 
             UIColor.white.setStroke()
-            rPath.lineWidth = 1
+            rPath.lineWidth = 1 / catalystTransform
             rPath.stroke()
 
 
             //// L Drawing
-            let lPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + 2, y: frame.minY + fastFloor((frame.height - 2) * 0.50000 + 0.5), width: 5, height: 2), cornerRadius: 0.5)
+            let lPath = UIBezierPath(roundedRect: CGRect(x: frame.minX + crosshairStrokePadding, y: frame.minY + fastFloor((frame.height - crosshairStrokeWidth) * 0.50000 + 0.5), width: crosshairStrokeLength, height: crosshairStrokeWidth), cornerRadius: 0.5)
             context.saveGState()
             context.setShadow(offset: shadow.shadowOffset, blur: shadow.shadowBlurRadius, color: (shadow.shadowColor as! UIColor).cgColor)
             color2.setFill()
@@ -184,7 +160,7 @@ class ViewController: UIViewController {
             context.restoreGState()
 
             UIColor.white.setStroke()
-            lPath.lineWidth = 1
+            lPath.lineWidth = 1 / catalystTransform
             lPath.stroke()
         }
         if let data = image??.pngData() {
